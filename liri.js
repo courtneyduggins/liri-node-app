37f72cc70aa559407d3b17acee1a3b8b734923dc
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-09-07 22:19:48 -0400

    add command for movie-this

--- liri.js
+++ liri.js
@@ -1,10 +1,26 @@
 var request = require("dotenv").config();
 var request = require('request');
+var nodeArgs = process.argv;
 
 // var keys = require("./key.js")
 // var spotify = new Spotify(keys.spotify);
 // var Spotify = require('node-spotify-api');
 
+var command = nodeArgs[2];
+// console.log(command);
+
+var input = "";
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+for (i = 3; i < nodeArgs[i]; i++) {
+
+    input = input + " " + nodeArgs[i];
+}
+
+input = input.trim().replace(" ", "+");
 
 
 // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
@@ -17,58 +33,66 @@ var request = require('request');
 
 //Request function for OMDB...
 
-var nodeArgs = process.argv;
 
-var movieName = "";
+if (command === "movie-this") {
 
-for (var i = 2; i < nodeArgs.length; i++) {
+    var movieName = "";
 
-    if (i > 2 && i < nodeArgs.length) {
 
-        movieName = movieName + "+" + nodeArgs[i];
+    for (var i = 2; i < nodeArgs.length; i++) {
 
-    }
+        if (i > 2 && i < nodeArgs.length) {
 
-    else {
-
-        movieName += nodeArgs[i];
-
-    }
-}
-
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-request(queryUrl, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-
-        // console.log(body);
-
-        //* Title of the movie.
-        //* Year the movie came out.
-        //* IMDB Rating of the movie.
-
-        for (i = 0; i < JSON.parse(body).Ratings.length; i++) {
-
-            if (JSON.parse(body).Ratings[i].Source === "Internet Movie Database") {
-
-                var IMBDrating = JSON.parse(body).Ratings[i].Value;
-
-            } 
-            
-            if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-
-                var RottenTomatoes = JSON.parse(body).Ratings[i].Value;
-            }
+            movieName = movieName + "+" + nodeArgs[i];
 
         }
-        //* Rotten Tomatoes Rating of the movie.
-        //* Country where the movie was produced
-        //* Language of the movie.
-        //* Plot of the movie.
-        //* Actors in the movie.
 
-        console.log(`
+        else {
+
+            movieName += nodeArgs[i];
+
+        }
+    }
+
+    if (movieName === "") {
+
+        movieName = "Mr. Nobody";
+    }
+
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            // console.log(body);
+
+            //* Title of the movie.
+            //* Year the movie came out.
+            //* IMDB Rating of the movie.
+
+            for (i = 0; i < JSON.parse(body).Ratings.length; i++) {
+
+                if (JSON.parse(body).Ratings[i].Source === "Internet Movie Database") {
+
+                    var IMBDrating = JSON.parse(body).Ratings[i].Value;
+
+                }
+
+                if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
+
+                    var RottenTomatoes = JSON.parse(body).Ratings[i].Value;
+                }
+
+            }
+            //* Rotten Tomatoes Rating of the movie.
+            //* Country where the movie was produced
+            //* Language of the movie.
+            //* Plot of the movie.
+            //* Actors in the movie.
+
+            console.log(`
 
         *Movie Title: ${JSON.parse(body).Title}
         *Release Year: ${JSON.parse(body).Year}
@@ -83,9 +107,11 @@ request(queryUrl, function (error, response, body) {
 
 
 
-    }
+        }
 
-});
+    });
+
+}
 
 //Requet function for Bands In Town...
 // var request = require('request');

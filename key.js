99dc94c5efa7bf3c92c8758e82c3360378a832a4
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-09-08 16:27:03 -0400

    add code to perform Spotify search

--- key.js
+++ key.js
@@ -1,6 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: '252f996260524d3fadc4ac0e3bc3dd7e',
+  //id: process.env.SPOTIFY_ID,
+  secret: 'e9e8aa043e42451484a7ecbd3e75c7e7'
+  //secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,121 +1,177 @@
 var request = require("dotenv").config();
 var request = require('request');
-var nodeArgs = process.argv;
+var command = process.argv[2];
+// var search = process.argv.slice(3).join(" ");
 
-// var keys = require("./key.js")
-// var spotify = new Spotify(keys.spotify);
-// var Spotify = require('node-spotify-api');
+var keys = require("./key.js")
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
-var command = nodeArgs[2];
+
 // console.log(command);
 
-var input = "";
+
 // concert-this
 // spotify-this-song
 // movie-this
 // do-what-it-says
 
-for (i = 3; i < nodeArgs[i]; i++) {
-
-    input = input + " " + nodeArgs[i];
-}
-
-input = input.trim().replace(" ", "+");
-
-
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-
-// console.log(data); 
-// });
-
-//Request function for OMDB...
-
-
+//perform movie search if command is "movie-this"....
 if (command === "movie-this") {
 
     var movieName = "";
 
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 3; i < process.argv.length; i++) {
 
-    for (var i = 2; i < nodeArgs.length; i++) {
+        if (i > 3 && i < process.argv.length) {
 
-        if (i > 2 && i < nodeArgs.length) {
-
-            movieName = movieName + "+" + nodeArgs[i];
+            movieName = movieName + "+" + process.argv[i];
 
         }
 
         else {
 
-            movieName += nodeArgs[i];
+            movieName += process.argv[i];
 
         }
-        
-        if (movieName === "") {
-
-        movieName = "Mr. Nobody";
-        }
-        
     }
 
-    
+
+    if (!movieName) {
+
+        movieName = "Mr. Nobody";
+    }
 
 
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=7c887434";
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    console.log(queryUrl);
 
     request(queryUrl, function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
 
-            console.log(body);
 
+            // console.log(body);
+            var movieData = JSON.parse(body);
+            console.log(movieData);
 
-            for (i = 0; i < JSON.parse(body).Ratings.length; i++) {
+            for (i = 0; i < movieData.Ratings.length; i++) {
 
-                if (JSON.parse(body).Ratings[i].Source === "Internet Movie Database") {
+                if (movieData.Ratings[i].Source === "Internet Movie Database") {
 
-                    var IMBDrating = JSON.parse(body).Ratings[i].Value;
+                    var IMBDrating = movieData.Ratings[i].Value;
 
                 }
 
-                if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
+                if (movieData.Ratings[i].Source === "Rotten Tomatoes") {
 
-                    var RottenTomatoes = JSON.parse(body).Ratings[i].Value;
+                    var RottenTomatoes = movieData.Ratings[i].Value;
                 }
 
             }
 
-            // //* Title of the movie.
-            // //* Year the movie came out.
-            // //* IMDB Rating of the movie.
-            // //* Rotten Tomatoes Rating of the movie.
-            // //* Country where the movie was produced
-            // //* Language of the movie.
-            // //* Plot of the movie.
-            // //* Actors in the movie.
+            //         // //* Title of the movie.
+            //         // //* Year the movie came out.
+            //         // //* IMDB Rating of the movie.
+            //         //     ${IMBDrating}
+            //         // //* Rotten Tomatoes Rating of the movie.
+            //         //     ${RottenTomatoes}
+            //         // //* Country where the movie was produced
+            //         // //* Language of the movie.
+            //         // //* Plot of the movie.
+            //         // //* Actors in the movie.
 
             console.log(`
 
-                *Movie Title: ${JSON.parse(body).Title}
-                *Release Year: ${JSON.parse(body).Year}
-                *IMBD Rating: ${IMBDrating}
-                *Rotten Tomatoes Rating: ${RottenTomatoes}
-                *Produced In: ${JSON.parse(body).Country}
-                *Language: ${JSON.parse(body).Language}
-                *Plot: ${JSON.parse(body).Plot}
-                *Actors: ${JSON.parse(body).Actors}
-        
-            `);
+        *Movie Title: ${movieData.Title}
+        *Release Year: ${movieData.Year}
+        *IMBD Rating: ${IMBDrating}
+        *Rotten Tomotoes Raing: ${RottenTomatoes}
+        *Produced In: ${movieData.Country}
+        *Language: ${movieData.Language}
+        *Plot: ${movieData.Plot}
+        *Actors: ${movieData.Actors}
+        \n------------------------------------------------------------\n\n
+    `);
 
 
 
+        } else {
+            console.log(error);
         }
 
     });
 
+//perform search if command is "spotify-this-song"....    
+
+} else if (command === "spotify-this-song") {
+
+    var songTitle = "";
+
+    for (var i = 3; i < process.argv.length; i++) {
+
+        if (i > 3 && i < process.argv.length) {
+
+            songTitle = songTitle + "+" + process.argv[i];
+
+        }
+
+        else {
+
+            songTitle += process.argv[i];
+
+        }
+
+    }
+
+    if (!songTitle) {
+
+        songTitle = "The Sign";
+    }
+
+    spotify.search({ type: 'track', query: songTitle, limit: 6 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+
+
+            var songInfo = data.tracks.items[5];
+
+            // console.log(songInfo);
+            // var Artists = JSON.parse(songInfo.album.artists.length);
+
+            console.log(`
+            
+            Song Title: ${songInfo.name}
+            Artist(s): ${songInfo.artists[0].name}
+            
+            
+            `);
+        }
+
+          
+    });
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//Request function for OMDB...
+
+
+
 
 //Requet function for Bands In Town...
 // var request = require('request');
